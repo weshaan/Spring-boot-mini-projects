32edde0b2123c31138dc0a0b0f0a1345a4d782fa
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2026-01-26 11:22:38 +0530

    Delete crud directory

--- crud/src/main/java/com/example/crud/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
